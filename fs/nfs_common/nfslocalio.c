a8faacca6e06aa3bdacee32220750a9e93e69224
Mike Snitzer <snitzer@hammerspace.com>
2024-11-07 15:42:37 +0000

    NFS/localio: add dummy definition for nfsd_file
    Add dummy definition for nfsd_file in both nfslocalio.c and localio.c
    so various compilers (e.g. gcc 8.5.0 and 9.5.0) can be used. Otherwise
    RCU code (rcu_dereference and rcu_access_pointer) will dereference
    what should just be an opaque pointer (by using typeof(*ptr)).
    
    Fixes: 86e00412254a ("nfs: cache all open LOCALIO nfsd_file(s) in client")
    Tested-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>
    Tested-by: Pali Rohár <pali@kernel.org>
    Tested-by: Vincent Mailhol <mailhol.vincent@wanadoo.fr>
    Acked-by: Jeff Layton <jlayton@kernel.org>
    [snitzer: stop-gap until neilb's rcu fixes are finished]
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>

--- fs/nfs_common/nfslocalio.c
+++ fs/nfs_common/nfslocalio.c
@@ -271,6 +271,14 @@ struct nfsd_file *nfs_open_local_fh(nfs_uuid_t *uuid,
 }
 EXPORT_SYMBOL_GPL(nfs_open_local_fh);
 
+/*
+ * nfsd_file structure is purposely kept opaque to NFS client.
+ * This is a dummy definition to make RCU compilation happy.
+ */
+struct nfsd_file {
+	int undefined__;
+};
+
 void nfs_close_local_fh(struct nfs_file_localio *nfl)
 {
 	struct nfsd_file *ro_nf = NULL;
