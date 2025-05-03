ad93e16ccc6a3f84747c173bce74f66fe6eb9799
Jeff Layton <jlayton@kernel.org>
2025-05-03 15:59:23 -0400

    nfsd: add nfsd_vfs_create tracepoints
    Observe the start of file and directory creation for all NFS
    versions.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    (cherry picked from commit 85b0b1f217856b19f7fc7a6b3bf2adafdcaf056e)
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>

--- fs/nfsd/nfs3proc.c
+++ fs/nfsd/nfs3proc.c
@@ -14,6 +14,7 @@
 #include "xdr3.h"
 #include "vfs.h"
 #include "filecache.h"
+#include "trace.h"
 
 #define NFSDDBG_FACILITY		NFSDDBG_PROC
 
@@ -289,6 +290,8 @@ nfsd3_create_file(struct svc_rqst *rqstp, struct svc_fh *fhp,
 	struct inode *inode;
 	__be32 status;
 	int host_err;
+
+	trace_nfsd_vfs_create(rqstp, fhp, S_IFREG, argp->name, argp->len);
 
 	if (!nfsd3_time_in_range(iap))
 		return nfserr_inval;
