4ffb6ea0f022cf76a4967f8ae11475afc979d8bf
Keith Busch <kbusch@kernel.org>
2025-08-27 07:12:52 -0700

    block: add size alignment to bio_iov_iter_get_pages
    The block layer tries to align bio vectors to the block device's logical
    block size. Some cases don't have a block device, or we may need to
    align to something larger, which we can't derive it from the queue
    limits. Have the caller specify what they want, or allow any length
    alignment if nothing was specified. Since the most common use case
    relies on the block device's limits, a helper function is provided.
    
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    [snitzer: hand-patched due to not having 3d8b5a22d40435 and 10b1e59cdadabf]
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>

--- include/linux/bio.h
+++ include/linux/bio.h
@@ -427,7 +427,14 @@ void bio_add_folio_nofail(struct bio *bio, struct folio *folio, size_t len,
 			  size_t off);
 void bio_add_virt_nofail(struct bio *bio, void *vaddr, unsigned len);
 
-int bio_iov_iter_get_pages(struct bio *bio, struct iov_iter *iter);
+int bio_iov_iter_get_pages_aligned(struct bio *bio, struct iov_iter *iter,
+		unsigned len_align_mask);
+
+static inline int bio_iov_iter_get_pages(struct bio *bio, struct iov_iter *iter)
+{
+	return bio_iov_iter_get_pages_aligned(bio, iter, 0);
+}
+
 void bio_iov_bvec_set(struct bio *bio, struct iov_iter *iter);
 void __bio_release_pages(struct bio *bio, bool mark_dirty);
 extern void bio_set_pages_dirty(struct bio *bio);
