15021d9d6e47b429451c7b19536c5d55f1743863
Ming Lei <ming.lei@redhat.com>
2025-12-31 11:00:56 +0800

    block: don't initialize bi_vcnt for cloned bio in bio_iov_bvec_set()
    bio_iov_bvec_set() creates a cloned bio that borrows a bvec array from
    an iov_iter. For cloned bios, bi_vcnt is meaningless because iteration
    is controlled entirely by bi_iter (bi_idx, bi_size, bi_bvec_done), not
    by bi_vcnt. Remove the incorrect bi_vcnt assignment.
    
    Explicitly initialize bi_iter.bi_idx to 0 to ensure iteration starts
    at the first bvec. While bi_idx is typically already zero from bio
    initialization, making this explicit improves clarity and correctness.
    
    This change also avoids accessing iter->nr_segs, which is an iov_iter
    implementation detail that block code should not depend on.
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Reviewed-by: Nitesh Shetty <nj.shetty@samsung.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 641864314866dff382f64cd8b52fd6bf4c4d84f6)
    [snitzer: hand-patched because don't have commit cafd00d0e909]
    Signed-off-by: Mike Snitzer <snitzer@hammerspace.com>

--- block/bio.c
+++ block/bio.c
@@ -1243,8 +1243,8 @@ void bio_iov_bvec_set(struct bio *bio, struct iov_iter *iter)
 		size = min(size, max_sectors << SECTOR_SHIFT);
 	}
 
-	bio->bi_vcnt = iter->nr_segs;
 	bio->bi_io_vec = (struct bio_vec *)iter->bvec;
+	bio->bi_iter.bi_idx = 0;
 	bio->bi_iter.bi_bvec_done = iter->iov_offset;
 	bio->bi_iter.bi_size = size;
 	bio_set_flag(bio, BIO_CLONED);
