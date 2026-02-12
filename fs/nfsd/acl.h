4c56dae20738c15b315fe6afb9815ae17e4d2a5b
Mike Snitzer <snitzer@hammerspace.com>
2026-02-12 17:52:47 +0000

    NFSD: add NFS4 reexport support for GETACL nfs4_acl passthru
    Allow NFSD's 4.1 reexport of a 4.2 mount to perform GETACL by passing
    thru nfs4_acl whose pages are allocated in nfsd4_get_nfs4_acl_passthru
    and then passed down to exported filesystem's ops->getacl(). Once
    nfs4_acl is retrieved nfsd4_encode_fattr4_acl() will send the
    ACL payload to the client using nfsd4_encode_nfs4_acl_passthru().
    
    Signed-off-by: Mike Snitzer <snitzer@hammerspace.com>

--- fs/nfsd/acl.h
+++ fs/nfsd/acl.h
@@ -42,13 +42,14 @@ struct svc_fh;
 struct svc_rqst;
 struct nfsd_attrs;
 enum nfs_ftype4;
+enum nfs4_acl_type;
 
 int nfs4_acl_bytes(int entries);
 int nfs4_acl_get_whotype(char *, u32);
 __be32 nfs4_acl_write_who(struct xdr_stream *xdr, int who);
 
 int nfsd4_get_nfs4_acl(struct svc_rqst *rqstp, struct dentry *dentry,
-		struct nfs4_acl **acl);
+		enum nfs4_acl_type acl_type, struct nfs4_acl **acl);
 __be32 nfsd4_acl_to_attr(enum nfs_ftype4 type, struct nfs4_acl *acl,
 			 struct nfsd_attrs *attr);
 void sort_pacl_range(struct posix_acl *pacl, int start, int end);
