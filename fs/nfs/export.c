a49edfa91e335ec9bc28449d43ba0b21e2c39c38
Mike Snitzer <snitzer@hammerspace.com>
2026-02-12 21:32:13 +0000

    NFSv4: set EXPORT_OP_NFSV4_ACL_PASSTHRU flag
    NFSv4 implements both .setacl and .getacl and so it is appropriate to
    enable nfs4_acl passthru.
    
    Signed-off-by: Mike Snitzer <snitzer@hammerspace.com>

--- fs/nfs/export.c
+++ fs/nfs/export.c
@@ -182,5 +182,6 @@ const struct export_operations nfs_export_ops = {
 		 EXPORT_OP_REMOTE_FS		|
 		 EXPORT_OP_NOATOMIC_ATTR	|
 		 EXPORT_OP_FLUSH_ON_CLOSE	|
-		 EXPORT_OP_NOLOCKS,
+		 EXPORT_OP_NOLOCKS              |
+		 EXPORT_OP_NFSV4_ACL_PASSTHRU,
 };
